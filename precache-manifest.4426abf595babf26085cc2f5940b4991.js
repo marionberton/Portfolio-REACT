292b9bec571f603b6192638cd52fa8e929ca3bcd
“Marion <marionberton03@gmail.com>
2019-12-19 12:48:41 +0000

    Updates

---
+++ precache-manifest.4426abf595babf26085cc2f5940b4991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13240cb3c17b360e6cf30d818d5a334b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dec150ce3e1e6a6e9f0a",
+    "url": "/my-app/static/css/main.4e557236.chunk.css"
+  },
+  {
+    "revision": "3df11086bd414518efac",
+    "url": "/my-app/static/js/2.44385c03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.44385c03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dec150ce3e1e6a6e9f0a",
+    "url": "/my-app/static/js/main.36098f10.chunk.js"
+  },
+  {
+    "revision": "1d0d1868f76663eec549",
+    "url": "/my-app/static/js/runtime-main.467216da.js"
+  }
+]);
